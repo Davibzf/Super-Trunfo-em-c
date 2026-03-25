c5ddbf27d275f7cab5004e24e79f8aad346cda79
Davi Bezerra Fraga <davibezerrafraga@gmail.com>
2026-03-25 15:47:09 -0300

    Update Programa.c

--- C/Programa.c
+++ C/Programa.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    char nome[100];
+    printf("Qual seu nome?\n");
+    scanf("%s", nome);
+    printf("Olá, %s! Seja bem-vindo ao mundo da programação em C!", nome);
     return 0;
 }
