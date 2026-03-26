bf3e58ab7f417c8516bf4631c1056a866a338c83
Davi Bezerra Fraga <davibezerrafraga@gmail.com>
2026-03-26 10:57:29 -0300

    soma de 3 numeros

---
+++ C/soma de 3 num.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+ 
+int main() {
+    int num1, num2, num3, soma;
+    printf("Digite o 1 numero: \n");
+    scanf("%d", &num1);
+
+    printf("Digite o 2 numero: \n");
+    scanf("%d", &num2);
+
+    printf("Digite o 3 numero: \n");
+    scanf("%d", &num3);
+
+    soma = num1 + num2 + num3;
+
+    printf("soma de %d + %d + %d = %d", num1, num2, num3, soma);
+    return 0;
+}
