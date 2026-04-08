6a6c04e29f8e497693a76624ab8f01c9bba0c695
Davi Bezerra Fraga <davibezerrafraga@gmail.com>
2026-04-08 15:19:36 -0300

    pessoas

--- C/aula5/pessoas.c
+++ C/aula5/pessoas.c
@@ -1,10 +1,27 @@
 #include <stdio.h>
 
 int main() {
-    char nome[] = "Davi";
-    int idade = 17;
-    char sexo = 'M';
-    char cidade[] = "Caucaia";
-    printf("O seu nome é %s.\nTem %i anos\nSeu sexo é %c\nMora em %s", nome, idade, sexo, cidade);
+    char nome[100];
+    int idade;
+    float altura;
+    char sexo[10];
+    char cidade[100];
+
+    printf("Digite seu nome: ");
+    scanf("%s", nome);
+
+    printf("Digite sua idade: ");
+    scanf("%d", &idade);
+    
+    printf("Digite a sua altura: ");
+    scanf("%f", &altura);
+
+    printf("Digite seu sexo: ");
+    scanf("%s", sexo);
+
+    printf("Digite a cidade onde mora: ");
+    scanf("%s", cidade);
+    
+    printf("O seu nome é %s.\nTem %i anos\nSua altura é %.2f\nSeu sexo é %s\nMora em %s", nome, idade, altura, sexo, cidade);
     return 0;
 }
