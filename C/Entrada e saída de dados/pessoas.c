407429219b49ef27cb68bd3f9f3cf997ccac456c
Davi Bezerra Fraga <davibezerrafraga@gmail.com>
2026-04-08 14:13:10 -0300

    Entrada e saída de dados

---
+++ C/Entrada e saída de dados/pessoas.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    char nome[] = "Davi";
+    int idade = 17;
+    char sexo = 'M';
+    char cidade[] = "Caucaia";
+    printf("O seu nome é %s, tem %i anos, e seu sexo é %c, e mora em %s", nome, idade, sexo, cidade);
+    return 0;
+}
