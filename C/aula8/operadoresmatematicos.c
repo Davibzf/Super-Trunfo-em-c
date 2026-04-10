2bc29a5069013ef906c6385a4620d7f904efb2ce
Davi Bezerra Fraga <davibezerrafraga@gmail.com>
2026-04-10 15:54:41 -0300

    ide

--- C/aula8/operadoresmatematicos.c
+++ C/aula8/operadoresmatematicos.c
@@ -3,11 +3,8 @@
 int main() {
     int num1, num2, soma, subtracao, multiplicacao, divisao;
 
-    printf("Digite o primeiro número: ");
-    scanf("%d", &num1);
-    
-    printf("Digite o segundo número: ");
-    scanf("%d", &num2);
+    num1 = 1;
+    num2 = 2;
 
     soma = num1 + num2;
     subtracao = num1 - num2;
