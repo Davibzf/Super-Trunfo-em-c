23a846c549e0e8c387ef5b370185d1385db82707
Davi Bezerra Fraga <davibezerrafraga@gmail.com>
2026-04-14 13:53:29 -0300

    notas

--- C/aula10/media.c
+++ C/aula10/media.c
@@ -2,18 +2,19 @@
 #include <windows.h>
 
 int main(){
-    float nota1, nota2, nota3, media;
+    int nota1, nota2, nota3;
+    float media;
 
     printf("Digite a primeira nota: \n");
-    scanf("%f", &nota1);
+    scanf("%d", &nota1);
 
     printf("Digite a sugunda nota: \n");
-    scanf("%f", &nota2);
+    scanf("%d", &nota2);
 
     printf("Digite a terceira nota: \n");
-    scanf("%f", &nota3);
+    scanf("%d", &nota3);
 
-    media = (nota1 + nota2 + nota3) /3 ;
+    media = (float)(nota1 + nota2 + nota3) /3 ;
 
     printf("A Média do aluno foi: %.2f", media);
 
